7e71ab7c39d930a144b55bd002bc0b77d84a1360
ypwhs <915505626@qq.com>
2015-05-13 15:07:22 +0800

    master
    OLED finished

--- Project/test/test/src/main.c
+++ Project/test/test/src/main.c
@@ -1,126 +1,85 @@
 #include "gpio.h"
 #include "common.h"
 #include "uart.h"
-#include "i2c.h"
+#include "spi.h"
 
-
-void WriteCommand(unsigned char ins)
+void Write_SPI_Command(unsigned char data){
+    PEout(4) = 0;
+    SPI_ReadWriteByte(HW_SPI1, HW_CTAR0, data, HW_SPI_CS0, kSPI_PCS_ReturnInactive);
+}
+void Write_SPI_Data(unsigned char data){
+    PEout(4) = 1;
+    SPI_ReadWriteByte(HW_SPI1, HW_CTAR0, data, HW_SPI_CS0, kSPI_PCS_ReturnInactive);
+}
+void fill_picture(unsigned char fill_Data)
 {
-    I2C_Start();
-    I2C_SendByte(0x00);I2C_WaitAck();
-    I2C_SendByte(ins);I2C_WaitAck();
-    I2C_Stop();
-//    I2C_WriteSingleRegister(HW_I2C0, 0x78>>1, 0x00, ins);
+	unsigned char m,n;
+	for(m=0;m<8;m++)
+	{
+		Write_SPI_Command(0xb0+m);		//page0-page1
+		Write_SPI_Command(0x00);		//low column start address
+		Write_SPI_Command(0x10);		//high column start address
+		for(n=0;n<132;n++)
+			{
+				Write_SPI_Data(fill_Data);
+			}
+	}
 }
-void WriteData(unsigned char dat)
-{
-    I2C_Start();
-    I2C_SendByte(0x40);I2C_WaitAck();
-    I2C_SendByte(dat);I2C_WaitAck();
-    I2C_Stop();
-//    I2C_WriteSingleRegister(HW_I2C0, 0x78>>1, 0x40, dat);
+void initOLED(){
+    SPI_QuickInit(SPI1_SCK_PE02_SOUT_PE01_SIN_PE03, kSPI_CPOL0_CPHA0, 8*1000*1000);
+    //PORT_PinMuxConfig(HW_GPIOC, 3, kPinAlt2);
+	Write_SPI_Command(0xAE);   //display off
+	Write_SPI_Command(0x20);	//Set Memory Addressing Mode
+	Write_SPI_Command(0x10);	//00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
+	Write_SPI_Command(0xb0);	//Set Page Start Address for Page Addressing Mode,0-7
+	Write_SPI_Command(0xc8);	//Set COM Output Scan Direction
+	Write_SPI_Command(0x00);//---set low column address
+	Write_SPI_Command(0x10);//---set high column address
+	Write_SPI_Command(0x40);//--set start line address
+	Write_SPI_Command(0x81);//--set contrast control register
+	Write_SPI_Command(0x7f);
+	Write_SPI_Command(0xa1);//--set segment re-map 0 to 127
+	Write_SPI_Command(0xa6);//--set normal display
+	Write_SPI_Command(0xa8);//--set multiplex ratio(1 to 64)
+	Write_SPI_Command(0x3F);//
+	Write_SPI_Command(0xa4);//0xa4,Output follows RAM content;0xa5,Output ignores RAM content
+	Write_SPI_Command(0xd3);//-set display offset
+	Write_SPI_Command(0x00);//-not offset
+	Write_SPI_Command(0xd5);//--set display clock divide ratio/oscillator frequency
+	Write_SPI_Command(0xf0);//--set divide ratio
+	Write_SPI_Command(0xd9);//--set pre-charge period
+	Write_SPI_Command(0x22); //
+	Write_SPI_Command(0xda);//--set com pins hardware configuration
+	Write_SPI_Command(0x12);
+	Write_SPI_Command(0xdb);//--set vcomh
+	Write_SPI_Command(0x20);//0x20,0.77xVcc
+	Write_SPI_Command(0x8d);//--set DC-DC enable
+	Write_SPI_Command(0x14);//
+	Write_SPI_Command(0xaf);//--turn on oled panel
 }
-void IIC_SetPos(unsigned char x, unsigned char y)
-{
-  WriteCommand(0xb0 + y);
-  WriteCommand(((x & 0xf0) >> 4) | 0x10); //|0x10
-  WriteCommand((x & 0x0f) | 0x01); //|0x01
-}
-//全屏显示 -- Fill_Screen(0x00,0x00)可用作清屏
-void Fill_Screen(unsigned char dat1, unsigned char dat2)
-{
-  unsigned char x, y;
-
-  WriteCommand(0x02);    /*set lower column address*/
-  WriteCommand(0x10);    /*set higher column address*/
-  WriteCommand(0xB0);    /*set page address*/
-  for (y = 0; y < 8; y++)
-  {
-    WriteCommand(0xB0 + y);  /*set page address*/
-    WriteCommand(0x02);    /*set lower column address*/
-    WriteCommand(0x10);    /*set higher column address*/
-
-    for (x = 0; x < 64; x++)
-    {
-      WriteData(dat1);
-      WriteData(dat2);
-    }
-  }
-}
-void initOLED() {
-    I2C_Scan(I2C0_SCL_PB00_SDA_PB01);
-    uint32_t instance = I2C_QuickInit(I2C0_SCL_PB00_SDA_PB01, 100*1000);
-    int r = I2C_Probe(instance, 0x78>>1);
-    if(r)
-        printf("OLED connect success\n");
-    else
-        printf("I2C init failed\n");
-
-  WriteCommand(0xAE);    /*display off*/
-
-  WriteCommand(0x02);    /*set lower column address*/
-  WriteCommand(0x10);    /*set higher column address*/
-
-  WriteCommand(0x40);    /*set display start line*/
-
-  WriteCommand(0xB0);    /*set page address*/
-
-  WriteCommand(0x81);    /*contract control*/
-  WriteCommand(0x80);    /*128*/
-
-  WriteCommand(0xA1);    /*set segment remap*/
-
-  WriteCommand(0xA6);    /*normal / reverse*/
-
-  WriteCommand(0xA8);    /*multiplex ratio*/
-  WriteCommand(0x3F);    /*duty = 1/32*/
-
-  WriteCommand(0xad);    /*set charge pump enable*/
-  WriteCommand(0x8b);     /*    0x8a    外供VCC   */
-
-  WriteCommand(0x30);    /*0X30---0X33  set VPP   9V 电压可以调整亮度!!!!*/
-
-  WriteCommand(0xC8);    /*Com scan direction*/
-
-  WriteCommand(0xD3);    /*set display offset*/
-  WriteCommand(0x00);   /*   0x20  */
-
-  WriteCommand(0xD5);    /*set osc division*/
-  WriteCommand(0x80);
-
-  WriteCommand(0xD9);    /*set pre-charge period*/
-  WriteCommand(0x1f);    /*0x22*/
-
-  WriteCommand(0xDA);    /*set COM pins*/
-  WriteCommand(0x12);//0x02 -- duanhang xianshi,0x12 -- lianxuhang xianshi!!!!!!!!!
-
-  WriteCommand(0xdb);    /*set vcomh*/
-  WriteCommand(0x40);
-
-
-  WriteCommand(0xAF);    /*display ON*/
-}
-
 
 int main(void)
 {
     DelayInit();
     /* 打印串口及小灯 */
     GPIO_QuickInit(HW_GPIOC, 3, kGPIO_Mode_OPP);
+    GPIO_QuickInit(HW_GPIOE, 4, kGPIO_Mode_OPP);
+    GPIO_QuickInit(HW_GPIOE, 5, kGPIO_Mode_OPP);
+    PEout(5) = 0;
+    DelayMs(1);
+    PEout(5) = 1;
     UART_QuickInit(UART0_RX_PB16_TX_PB17, 115200);
 
     printf("OLED test\r\n");
 
     initOLED();
-    Fill_Screen(0x00, 0x00);
-
+    DelayMs(1);
     while(1)
     {
         GPIO_ToggleBit(HW_GPIOC, 3);
-      IIC_SetPos(0, 0);
-      Fill_Screen(0xAA, 0x55);
+      fill_picture(0xFF);
       DelayMs(500);
-      Fill_Screen(0x00, 0x00);
+      fill_picture(0x00);
       DelayMs(500);
     }
 }

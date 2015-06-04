5fb54ae8f83cee3d6615c07281646d85247a3e7f
ypw <915505626@qq.com>
2015-06-04 22:57:56 +0800

    1

--- Project/test/test/src/main.c
+++ Project/test/test/src/main.c
@@ -7,14 +7,14 @@
 
 void turn(int angel){
     int pwm = (int)((angel/180.0 + 1) * 500);  //90度是1.5ms，0度是1ms，180度是2ms
-    FTM_PWM_ChangeDuty(HW_FTM0, HW_FTM_CH3, pwm);
+    FTM_PWM_ChangeDuty(HW_FTM3, HW_FTM_CH4, pwm);
 }
-#define DRIVER_PWM_WIDTH 5000
+#define DRIVER_PWM_WIDTH 10000
 void initDriver(){
     printf("initPWM\r\n");
-    for(int i=0;i<8;i++)
-        GPIO_QuickInit(HW_GPIOC, i, kGPIO_Mode_OPP);
 
+    for(int i=0;i<0;i++)
+        GPIO_QuickInit(HW_GPIOC, i, kGPIO_Mode_OPP);
     //初始化所有引脚为输出，其中
     /*
         C0----INH
@@ -26,8 +26,11 @@ void initDriver(){
         C6----LED2
         C7----LED3
     */
-    //FTM_PWM_QuickInit(FTM0_CH3_PA06, kPWM_EdgeAligned, 50);     //设置频率50HZ，20ms脉宽，边沿对齐模式
-    //FTM_PWM_ChangeDuty(HW_FTM0, HW_FTM_CH3, 750);              //设置初始占空比750/10000*100% = 7.5% = 1.5ms
+    PCout(0)=1;
+    //使能INH
+
+    FTM_PWM_QuickInit(FTM3_CH4_PC08, kPWM_EdgeAligned, 50);     //设置频率50HZ，20ms脉宽，边沿对齐模式
+    turn(90);
     //初始化舵机
 
     FTM_PWM_QuickInit(FTM0_CH0_PC01, kPWM_EdgeAligned, DRIVER_PWM_WIDTH);
@@ -64,9 +67,11 @@ void delay(){
     DelayMs(500);
 }
 
+
+#define spd 1000
 //串口接收中断
 void UART_RX_ISR(uint16_t byteRec){
-#define spd 1000
+    PCout(3)=!PCout(3);
     switch(byteRec){
     case 'w':
         setLeftSpeed(spd);
@@ -77,16 +82,17 @@ void UART_RX_ISR(uint16_t byteRec){
         setRightSpeed(-spd);
         break;
     case 'a':
-        //turn(60);
+        turn(60);
         break;
     case 'd':
-        //turn(120);
+        turn(120);
         break;
     default:
         setLeftSpeed(0);
         setRightSpeed(0);
         break;
     }
+    printf("%c", byteRec);
 }
 
 int main(void)
@@ -96,7 +102,12 @@ int main(void)
     /* 打印串口及小灯 */
 
     GPIO_QuickInit(HW_GPIOC, 3, kGPIO_Mode_OPP);
+
     UART_QuickInit(UART0_RX_PB16_TX_PB17, 115200);
+    /*
+    UART0_RX_PB16_TX_PB17
+    UART4_RX_PC14_TX_PC15
+    */
 
     /* 注册中断回调函数 */
     UART_CallbackRxInstall(HW_UART0, UART_RX_ISR);
@@ -104,8 +115,8 @@ int main(void)
     /* 开启UART Rx中断 */
     UART_ITDMAConfig(HW_UART0, kUART_IT_Rx, true);
 
-    initDriver();
-    PCout(0)=1;
+    //initDriver();
+    FTM_PWM_QuickInit(FTM3_CH4_PC08, kPWM_EdgeAligned, 50);
 
     while(1)
     {
